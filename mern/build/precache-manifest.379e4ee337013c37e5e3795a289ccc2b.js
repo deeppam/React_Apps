c90d2ccecfe886c7547e72e1f6de3bf49b296777
deeparebba <deeppam@yahoo.com>
2020-11-07 23:13:31 -0500

    first commit

---
+++ mern/build/precache-manifest.379e4ee337013c37e5e3795a289ccc2b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e3c3d3d7fcaad70fd455b018f5c7f8cb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "645780ea8b61b0ba2e2f",
+    "url": "/static/css/main.b7bb2b6f.chunk.css"
+  },
+  {
+    "revision": "7d9065a736f782816e2d",
+    "url": "/static/js/2.a6ed427a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.a6ed427a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "645780ea8b61b0ba2e2f",
+    "url": "/static/js/main.fc82b326.chunk.js"
+  },
+  {
+    "revision": "0980db0307ffad65803b",
+    "url": "/static/js/runtime-main.df5858f9.js"
+  }
+]);
